ecdd11d35b5033ccce743ebbc0fda85694dbc2d9
rasha108bik <rasha108bick@yandex.ru>
2022-12-03 16:03:22 +0400

    add iter5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,26 +4,35 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 
+	"github.com/rasha108bik/tiny_url/internal/config"
 	"github.com/rasha108bik/tiny_url/internal/server"
 	"github.com/rasha108bik/tiny_url/internal/server/handlers"
 	"github.com/rasha108bik/tiny_url/internal/storage"
 )
 
 func main() {
-	r := chi.NewRouter()
+	var cfg config.Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	db := storage.NewStorage()
 	h := handlers.NewHandler(db)
 	serv := server.NewServer(h)
 
+	r := chi.NewRouter()
 	r.MethodNotAllowed(serv.Handlers.ErrorHandler)
-	r.Get("/{id}", serv.Handlers.GetOriginalURL)
-	r.Post("/", serv.Handlers.CreateShortLink)
-	r.Post("/api/shorten", serv.Handlers.CreateShorten)
+	r.Route(cfg.BaseURL, func(r chi.Router) {
+		r.Get("/{id}", serv.Handlers.GetOriginalURL)
+		r.Post("/", serv.Handlers.CreateShortLink)
+		r.Post("/api/shorten", serv.Handlers.CreateShorten)
+	})
 
-	err := http.ListenAndServe("127.0.0.1:8080", r)
+	err = http.ListenAndServe(cfg.ServerAddress, r)
 	if err != nil {
 		log.Fatal(err)
 	}
