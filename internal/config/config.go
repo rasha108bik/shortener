20f4133ede33c80ad5ed61b693d776f28fa8ba60
rasha108bik <rasha108bick@yandex.ru>
2022-12-04 15:25:29 +0400

    fix titer5-2

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,5 +2,5 @@ package config
 
 type Config struct {
 	BaseURL       string `env:"BASE_URL" envDefault:"/"`
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"http://127.0.0.1:8080/"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 }
