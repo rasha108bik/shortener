1575aaec743add524cc90e216c2f51f0172f764d
rasha108bik <rasha108bick@yandex.ru>
2022-12-06 15:07:51 +0530

    fix config ServerAddress

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,5 +2,5 @@ package config
 
 type Config struct {
 	BaseURL       string `env:"BASE_URL" envDefault:"/"`
-	ServerAddress string `env:"PORT" envDefault:"8080"`
+	ServerAddress string `env:"PORT" envDefault:"127.0.0.1:8080"`
 }
