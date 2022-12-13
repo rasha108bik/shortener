a50a6e990a4a6b45fc1448b0e1dfbed0c87231d3
rasha108bik <rasha108bick@yandex.ru>
2022-12-13 11:14:59 +0530

    iter7

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -14,11 +15,37 @@ import (
 	"github.com/rasha108bik/tiny_url/pkg/storagefile"
 )
 
+var (
+	serverAddress   string
+	baseURL         string
+	fileStoragePath string
+)
+
+func init() {
+	flag.StringVar(&serverAddress, "a", "", "server address")
+	flag.StringVar(&baseURL, "b", "", "base URL")
+	flag.StringVar(&fileStoragePath, "f", "", "file storage path")
+}
+
 func main() {
+	flag.Parse()
+	log.Printf("server address: %s, base URL: %s, file storagePath: %s\n", serverAddress, baseURL, fileStoragePath)
+
+	// TODO: create app direction
 	var cfg config.Config
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if serverAddress != "" {
+		cfg.ServerAddress = serverAddress
+	}
+	if baseURL != "" {
+		cfg.BaseURL = baseURL
+	}
+	if fileStoragePath != "" {
+		cfg.FileStoragePath = fileStoragePath
 	}
 
 	log.Printf("%+v\n", cfg)
