a4729bcae0f078847f96587b300ad3cdcd42ef37
rasha108bik <rasha108bick@yandex.ru>
2023-01-09 17:22:44 +0800

    iter11 fix

--- migrations/000001_init_schema_tiny_url.up.sql
+++ migrations/000001_init_schema_tiny_url.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE short_links (
     id SERIAL PRIMARY KEY,
     -- user_id INT NOT NULL,
     short_url TEXT NOT NULL,
-    original_url TEXT NOT NULL,
+    original_url TEXT NOT NULL
     -- CONSTRAINT fk_users
     --     FOREIGN KEY(user_id)
     --         REFERENCES users(id)
