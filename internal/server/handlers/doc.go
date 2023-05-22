d122350f9f485e76093e2a107af0cdd31cf2a4d7
rasha108bik <rasha108bick@yandex.ru>
2023-05-22 20:55:24 +0300

    iter17

--- internal/server/handlers/doc.go
+++ internal/server/handlers/doc.go
@@ -1,4 +1,20 @@
 /*
 Package handlers provides all public API methods
+
+CreateShortLink create short link and save in DB
+CreateShorten create short link and save in DB with ReqCreateShorten model
+ErrorHandler method which not include API methods
+FetchURLs get all URLs from DB
+GetOriginalURL get URL which include in url param
+Ping is handler which check liveness server
+ShortenBatch save URLs which include ReqShortenBatch model
+
+Handlers all API public methods
+interface.
+
+NewHandler returns a newly initialized handler objects that implements the Handlers interface
+interface.
+
+DeleteURLs delete urls which include in reqBody
 */
 package handlers
