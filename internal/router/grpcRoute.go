4e3a63aad8dcc92303d9ecb77dfdea4e631fb375
rasha108bik <rasha108bick@yandex.ru>
2023-10-13 16:21:33 +0300

    iter24

--- internal/router/grpcRoute.go
+++ internal/router/grpcRoute.go
@@ -4,9 +4,11 @@ import (
 	"github.com/rasha108bik/tiny_url/internal/server/handlers"
 )
 
+// GRPCRoute struct for return constructor.
 type GRPCRoute struct {
 }
 
+// NewRouter returns a newly *RouterFacade objects that registery pattern and middleware.
 func newGRPCRoute(s handlers.Handlers) GRPCRoute {
 	return GRPCRoute{}
 }
